9cbc335d4fdb12a0b7e07b1d3c861b241f4c448a
codekendo <brandonwong@fastmail.com>
2017-08-01 11:44:15 -0700

    refactor:Refactored Searchbook by using a resuable component booksgrid

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './App.css'
 import {Link} from 'react-router-dom'
 import _ from 'lodash'
+import BooksGrid from './BooksGrid'
+
 
 class SearchBooks extends React.Component {
   constructor(props) {
@@ -48,43 +50,7 @@ class SearchBooks extends React.Component {
             </div>
           </div>
           <div className="search-books-results">
-            <ol className="books-grid">
-              {(this.state.query !== undefined && booksObject.length > 3) && (booksObject.map((book) => (
-                <li key={book.id}>
-                  <div className='book'>
-                    <div className="book-top">
-                      <div className='book-cover' style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${book.imageLinks.thumbnail})`
-                      }}></div>
-                      <div className="book-shelf-changer">
-                        <select onChange={this.handleChange} data-book={book.id} value={book.shelf}>
-                          <option value="none" disabled>Move to...</option>
-                          <option value="currentlyReading">{(book.shelf === 'currentlyReading') && '\u2714'}
-                            Currently Reading</option>
-                          <option value="wantToRead">
-                            {(book.shelf === 'wantToRead') && '\u2714'}
-                            Want to Read
-                          </option>
-                          <option value="read">
-                            {(book.shelf === 'read') && '\u2714'}
-                            Read
-                          </option>
-                          <option value="none">
-                            {(book.shelf === 'none') && '\u2714'}
-                            None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    {book.authors && (book.authors.map((author, item) => (
-                      <div key={item} className="book-authors">{author}</div>
-                    )))}
-                  </div>
-                </li>
-              )))}
-            </ol>
+            {(this.state.query) && <BooksGrid books={booksObject} onChangeProp={this.handleChange}/>}
           </div>
         </div>
       </div>
